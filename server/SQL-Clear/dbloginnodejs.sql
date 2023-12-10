fd06dc2abb8fd42fb619c2a9838933853659243b
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 19:50:27 -0300

    Ok

--- server/SQL-Clear/dbloginnodejs.sql
+++ server/SQL-Clear/dbloginnodejs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-08-2022 a las 17:58:17
+-- Tiempo de generación: 10-12-2023 a las 23:46:15
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
--- server/SQL-Full/dbloginnodejs.sql
+++ server/SQL-Full/dbloginnodejs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-08-2022 a las 23:28:57
+-- Tiempo de generación: 10-12-2023 a las 23:47:19
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
